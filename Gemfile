9f48d7d970dc018558b4a8449735dee4f5579f2f
Heiko W. Rupp <hwr@pilhuhn.de>
2016-03-17 14:57:57 +0100

    Add alerting.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'mqtt',            '~>0.3.0'
-gem 'hawkular-client', '~>0.2.1',  :git => "git@github.com:pilhuhn/hawkular-client-ruby.git", :branch => "master"
+# gem 'hawkular-client', '~>0.2.1',  :git => "git@github.com:hawkular/hawkular-client-ruby.git", :branch => "master"
+gem 'hawkular-client', '~>0.2.1'
+gem 'addressable'
--- lib/hawk.rb
+++ lib/hawk.rb
@@ -1,17 +1,40 @@
 require 'rubygems'
 require 'mqtt'
+require 'YAML'
 require 'hawkular_all'
 
-
-INVENTORY_BASE = 'http://localhost:8080/hawkular/inventory'
-METRICS_BASE = 'http://localhost:8080/hawkular/metrics'
+#----------put your settings here ----
+HAWKULAR_BASE = 'http://localhost:8080/hawkular'
 CREDS = {username: 'jdoe', password: 'password'}
 BROKER = 'snert'
+@webhook_props = { url: 'http://172.31.7.177/',
+                   method: 'POST' }
 
-@inv_client = Hawkular::Inventory::InventoryClient.new(INVENTORY_BASE, CREDS)
+#--------------------------------------
+
+INVENTORY_BASE = "#{HAWKULAR_BASE}/inventory"
+METRICS_BASE = "#{HAWKULAR_BASE}/metrics"
+ALERTS_BASE = "#{HAWKULAR_BASE}/alerts"
+
+hash={:credentials => CREDS}
+@inv_client = Hawkular::Inventory::InventoryClient.new(hash)
 @metrics_client = Hawkular::Metrics::Client.new(METRICS_BASE, CREDS)
+@alerts_client = Hawkular::Alerts::AlertsClient.new(ALERTS_BASE, CREDS)
 
 @queue = []
+
+def help_and_exit(extras=nil)
+  puts extras unless extras.nil?
+  puts 'Usage: ruby hawk.rb <config_file>'
+  exit 1
+end
+
+
+help_and_exit if $*.length < 1
+config_path = $*[0]
+help_and_exit "#{config_path} is not readable" unless File.readable?(config_path)
+
+@config = YAML.load_file(config_path)
 
 def process_metric(message)
   # data format is : id value [timestamp]
@@ -58,7 +81,7 @@ def register_resource(_topic, message)
   puts '=> Data ' + data.to_s
 
   feed = data['feed']
-  code = @inv_client.create_feed feed, feed # Set separate feed name
+  @inv_client.create_feed feed, feed # Set separate feed name
   puts 'Feed created: '
 
   type = @inv_client.create_resource_type feed, data['rt'], data['rt']
@@ -73,14 +96,83 @@ def register_resource(_topic, message)
   unit = m_type['unit']
   metric_type = @inv_client.create_metric_type feed, m_type['id'], d_type, unit, m_type['collectionInterval']
 
-  @inv_client.create_metric_for_resource feed, metric['id'], metric_type.path, resource.id, metric['na']
+  metric_id = metric['id']
+
+  # check if we have an override
+  m_def = @config.fetch metric_id if @config.key?( metric_id)
+#  name = m_def.key?(:name) ? m_def.fetch(:name) : metric['na']
+  name = metric['na'] # There is bug in the explorer :-(
+
+  @inv_client.create_metric_for_resource feed, metric_id, metric_type.path, resource.id, name
 
   # As we want a longer TTL, we also need to register in metrics for now :-(
   # we chose 90 days retention
-  @metrics_client.gauges.create(id: metric['id'], dataRetention:90)
+  begin
+    @metrics_client.gauges.create(id: metric_id, dataRetention:90)
+  rescue Exception => e
+    puts e.to_s
+  end
 
+  register_alert feed, resource.id, metric_id, m_def, name if m_def.key?(:alert)
 end
 
+def register_alert(feed, res_id, metric_id, m_def, name)
+
+  alert = m_def.fetch(:alert)
+
+  begin
+    puts 'Trying to create webhook action'
+    @alerts_client.create_action 'webhook', "send-via-webhook-#{metric_id}", @webhook_props
+
+  rescue Hawkular::BaseClient::HawkularException => e
+    if e.status_code == 409
+      puts 'Webhook action already existed'
+      @client.delete_action 'webhook', "send-via-webhook-#{metric_id}"
+      retry
+    end
+  end
+
+  # Create the trigger
+  t = Hawkular::Alerts::Trigger.new({})
+  t.enabled = true
+  t.id = "trigger_#{metric_id}"
+  t.name = "Trigger on #{name}"
+  t.severity = :HIGH
+  t.description = "High temp on #{name} (#{metric_id})"
+  t.tags = { :resourceId => "#{feed}/#{res_id}" }
+  t.type = :MEMBER
+  t.context = { :triggerType => :Threshold,
+                :alertType => :ABS_VALUE } # Fake, but UI does not accept all values
+
+  # Create a condition
+  c = Hawkular::Alerts::Trigger::Condition.new({})
+  c.trigger_mode = :FIRING
+  c.type = :THRESHOLD
+  c.data_id = metric_id
+  c.operator = alert[:comparator].to_s.upcase
+  c.threshold = alert[:value]
+
+  # Reference an action definition
+  a = Hawkular::Alerts::Trigger::Action.new({})
+  a.action_plugin = 'webhook'
+  a.action_id = "send-via-webhook-#{metric_id}"
+  t.actions.push a
+
+  begin
+    @alerts_client.delete_trigger t.id
+  rescue
+    puts 'Trigger did not yet exist'
+  end
+
+  begin
+    @alerts_client.create_trigger t, [c], nil
+    puts 'Trigger created'
+  rescue Exception => e
+    puts 'Trigger creation failed : ' + e.to_s
+  end
+
+
+end
 
 MQTT::Client.connect(BROKER) do |c|
 
@@ -94,5 +186,4 @@ MQTT::Client.connect(BROKER) do |c|
     register_resource(topic, message) if topic.eql? '/hawkular/register'
   end
 end
-
 
